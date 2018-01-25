d1e8e3db9f3fe877e0be08a1170f93646f69cb02
Miguel <miguelvascomacamo@gmail.com>
2018-01-25 13:52:30 +0200

    Organizando o codigo e chamando o progress Activity

--- app/src/main/java/com/example/miguel/teste/MainActivity.java
+++ app/src/main/java/com/example/miguel/teste/MainActivity.java
@@ -9,13 +9,22 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt1;
+    Button bt1, btprogress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // chamando os methods
+
+        secondAcrivity();
+        callingThirdActivity();
+    }
+
+
+
+    private void secondAcrivity () {
         bt1 = (Button) findViewById(R.id.button);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,11 +32,21 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent meuIntent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(meuIntent);
-
-               // Toast.makeText(getApplicationContext(), "Este eh o meu programa em android", Toast.LENGTH_LONG).show();
-
+                Toast.makeText(getApplicationContext(), "Este eh o meu programa em android", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void callingThirdActivity () {
+       btprogress = (Button) findViewById(R.id.btnprogress);
+
+       btprogress.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent progress = new Intent(MainActivity.this, ThirdActivity.class);
+               startActivity(progress);
+           }
+       });
 
     }
 }
