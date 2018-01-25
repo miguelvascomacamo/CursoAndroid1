b5775d1cf81bac7917e60db0fdcba09ec3150053
Miguel <miguelvascomacamo@gmail.com>
2018-01-25 11:47:32 +0200

    Crriando o metodo para fazer correr o  tostmessage

--- app/src/main/java/com/example/miguel/teste/MainActivity.java
+++ app/src/main/java/com/example/miguel/teste/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.miguel.teste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = (Button) findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Este eh o meu programa em android", Toast.LENGTH_LONG).show();
+               
+            }
+        });
+
     }
 }
